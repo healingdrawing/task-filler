59ad321fce4e191e8dfe6c0e7feea7c13e38ff0f
healingdrawing <healingdrawing@gmail.com>
2023-10-16 04:09:24 +0300

    add find_enemy_agressive

--- filler/src/find_enemy_agressive.rs
+++ filler/src/find_enemy_agressive.rs
@@ -3,12 +3,12 @@ use crate::{parse_::Parser, find_::{Finder, Compas}, debug::{append_to_file, DEB
 impl Finder {
   pub fn find_enemy_agressive(&mut self, parser: &mut Parser)-> [usize;2] {
     let enemy_char = &parser.enemy_char;
-    let mut  enemy_xy = [usize::MAX, usize::MAX];
     let anfield = &parser.anfield;
+    let mut  enemy_xy = [anfield[0].len()-1, anfield.len()-1];/* 0 because of -y is the direction for the enemy in case of N, so +y is the most agressive position for the enemy progress. 0 because of -x is the direction for the enemy in case of W, so +x is the most agressive position for the enemy progress.*/
     
     match self.major {
       Compas::N => {
-        enemy_xy = [usize::MAX, 0];/* 0 because of -y is the direction for the enemy in case of N, so +y is the most agressive position for the enemy progress. x is minor in case of N*/
+        enemy_xy[1] = 0;/* 0 because of -y is the direction for the enemy in case of N, so +y is the most agressive position for the enemy progress. x is minor in case of N*/
         for y in 0..anfield.len() {
           for x in 0..anfield[0].len() {
             if anfield[y][x] == enemy_char[0] || anfield[y][x] == enemy_char[1] { /*the cell is covered by enemy char */
@@ -33,7 +33,7 @@ impl Finder {
         }
       },
       Compas::W => {
-        enemy_xy = [0, usize::MAX];/* 0 because of -x is the direction for the enemy in case of W, so +x is the most agressive position for the enemy progress. y is minor in case of W*/
+        enemy_xy[0] = 0;/* 0 because of -x is the direction for the enemy in case of W, so +x is the most agressive position for the enemy progress. y is minor in case of W*/
         for y in 0..anfield.len() {
           for x in 0..anfield[0].len() {
             if anfield[y][x] == enemy_char[0] || anfield[y][x] == enemy_char[1] { /*the cell is covered by enemy char */
@@ -68,16 +68,66 @@ impl Finder {
               }
               else if x > enemy_xy[0] && y >= enemy_xy[1]
               || x >= enemy_xy[0] && y > enemy_xy[1] { /*at least one of coordinates is more agressively moved */
-                //todo: implement
                 /*here is myddy place, need some way to compare the distances, between present enemy_xy value and perhaps 0,0 and the new one, before set the new value. not clear */
+                if self.find_distance([0,0], [x,y]) > self.find_distance([0,0], enemy_xy) {
+                  enemy_xy = [x, y];
+                }
               }
             }
           }
         }
       },
-      Compas::NE => {},
-      Compas::SW => {},
-      Compas::SE => {},
+      Compas::NE => {
+        enemy_xy = [anfield[0].len()-1, 0];/* 0 because of -y is the direction for the enemy in case of NE, so +y is the most agressive position for the enemy progress. anfield[0].len()-1 because of +x is the direction for the enemy in case of NE, so -x is the most agressive position for the enemy progress.*/
+        for y in 0..anfield.len() {
+          for x in 0..anfield[0].len() {
+            if anfield[y][x] == enemy_char[0] || anfield[y][x] == enemy_char[1] { /*the cell is covered by enemy char */
+              if x < enemy_xy[0] && y > enemy_xy[1] {
+                enemy_xy = [x, y];
+              } else if x <= enemy_xy[0] && y > enemy_xy[1]
+              || x < enemy_xy[0] && y >= enemy_xy[1] { /*at least one of coordinates is more agressively moved */
+                if self.find_distance([anfield[0].len()-1,0], [x,y]) > self.find_distance([anfield[0].len()-1,0], enemy_xy) {
+                  enemy_xy = [x, y];
+                }
+              }
+            }
+          }
+        }
+      },
+      Compas::SW => {
+        enemy_xy = [0, anfield.len()-1];/* anfield.len()-1 because of +y is the direction for the enemy in case of SW, so -y is the most agressive position for the enemy progress. 0 because of -x is the direction for the enemy in case of SW, so +x is the most agressive position for the enemy progress.*/
+        for y in 0..anfield.len() {
+          for x in 0..anfield[0].len() {
+            if anfield[y][x] == enemy_char[0] || anfield[y][x] == enemy_char[1] { /*the cell is covered by enemy char */
+              if x > enemy_xy[0] && y < enemy_xy[1] {
+                enemy_xy = [x, y];
+              } else if x > enemy_xy[0] && y <= enemy_xy[1]
+              || x >= enemy_xy[0] && y < enemy_xy[1] { /*at least one of coordinates is more agressively moved */
+                if self.find_distance([0,anfield.len()-1], [x,y]) > self.find_distance([0,anfield.len()-1], enemy_xy) {
+                  enemy_xy = [x, y];
+                }
+              }
+            }
+          }
+        }
+      },
+      Compas::SE => {
+        enemy_xy = [anfield[0].len()-1, anfield.len()-1];/* anfield.len()-1 because of +y is the direction for the enemy in case of SE, so -y is the most agressive position for the enemy progress. anfield[0].len()-1 because of +x is the direction for the enemy in case of SE, so -x is the most agressive position for the enemy progress.*/
+        for y in 0..anfield.len() {
+          for x in 0..anfield[0].len() {
+            if anfield[y][x] == enemy_char[0] || anfield[y][x] == enemy_char[1] { /*the cell is covered by enemy char */
+              if x < enemy_xy[0] && y < enemy_xy[1] {
+                enemy_xy = [x, y];
+              } else if x <= enemy_xy[0] && y < enemy_xy[1]
+              || x < enemy_xy[0] && y <= enemy_xy[1] { /*at least one of coordinates is more agressively moved */
+                if self.find_distance([anfield[0].len()-1,anfield.len()-1], [x,y]) > self.find_distance([anfield[0].len()-1,anfield.len()-1], enemy_xy) {
+                  enemy_xy = [x, y];
+                }
+              }
+            }
+          }
+        }
+      },
       Compas::CENTRAL => {append_to_file(DEBUG_FILE, &"fail in find_enemy_agressive. Should never fire".to_string()).expect("Unable to write data")},
     }
     enemy_xy
